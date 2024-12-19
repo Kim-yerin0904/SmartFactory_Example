423a616cdf2a60cf9825d1ed7ebf8a99015caec3
Kim-yerin0904 <yeelin0904@naver.com>
2024-12-19 12:39:23 +0900

    update switch 실습

--- example_241219/Form1.cs
+++ example_241219/Form1.cs
@@ -39,31 +39,31 @@ namespace example_241219
                 switch (dayenum)
                 {
                     case sevenDays.월요일:
-                        textBoxResult.Text = "월요일은 ~";
+                        textBoxResult.Text = "월요일은 분명 출근하긴 했는데 뭐했는지 기억이 없는 날";
                         break;
                     case sevenDays.화요일:
-                        textBoxResult.Text = "화요일";
+                        textBoxResult.Text = "화요일은 화장실 가는 척하면서 집 가고 싶은 날";
                         break;
                     case sevenDays.수요일:
-                        textBoxResult.Text = "수요일";
+                        textBoxResult.Text = "수요일은 수틀리면 사직서 내고 싶은 날";
                         break;
                     case sevenDays.목요일:
-                        textBoxResult.Text = "목요일";
+                        textBoxResult.Text = "목요일은 금요일인 줄 알고 있다가 아닌 거 알고 혼자 화나는 날";
                         break;
                     case sevenDays.금요일:
-                        textBoxResult.Text = "금요일";
+                        textBoxResult.Text = "금요일 모든 일이 안됐지만 월요일의 나에게 부탁하고 튀는 날";
                         break;
                     case sevenDays.토요일:
-                        textBoxResult.Text = "토요일";
+                        textBoxResult.Text = "토요일은 나갈까 말까 고민하다가 결국 아무것도 안 하는 날";
                         break;
                     case sevenDays.일요일:
-                        textBoxResult.Text = "일요일";
+                        textBoxResult.Text = "일요일은 주말인데 괜히 심란한 날";
                         break;
                 }
             }
             else
             {
-                textBoxResult.Text = "요일만 입력해주세요! ex.수요일/";
+                textBoxResult.Text = "요일만 입력해주세요! ex.수요일";
             }
                 
         }
