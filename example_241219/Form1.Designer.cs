77778b6eacf2813bed09807f2b9ca1dbf3cf0c20
Kim-yerin0904 <yeelin0904@naver.com>
2024-12-19 12:35:05 +0900

    switch 실습

---
+++ example_241219/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace example_241219
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(59, 92);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(100, 25);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "input";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(59, 146);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(357, 134);
+            this.textBoxResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxResult;
+    }
+}
+
---
+++ example_241219/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace example_241219
+{
+    public partial class Form1 : Form
+    {
+        // Form1()은 생성 시 최초 1회만 실행
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public enum sevenDays
+        {
+            월요일,
+            화요일,
+            수요일,
+            목요일,
+            금요일,
+            토요일,
+            일요일
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] days = { "월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일" };
+            if (days.Contains(textBoxInput.Text))
+            {
+                string day = textBoxInput.Text;
+                sevenDays dayenum = (sevenDays)Enum.Parse(typeof(sevenDays), day);
+                switch (dayenum)
+                {
+                    case sevenDays.월요일:
+                        textBoxResult.Text = "월요일은 ~";
+                        break;
+                    case sevenDays.화요일:
+                        textBoxResult.Text = "화요일";
+                        break;
+                    case sevenDays.수요일:
+                        textBoxResult.Text = "수요일";
+                        break;
+                    case sevenDays.목요일:
+                        textBoxResult.Text = "목요일";
+                        break;
+                    case sevenDays.금요일:
+                        textBoxResult.Text = "금요일";
+                        break;
+                    case sevenDays.토요일:
+                        textBoxResult.Text = "토요일";
+                        break;
+                    case sevenDays.일요일:
+                        textBoxResult.Text = "일요일";
+                        break;
+                }
+            }
+            else
+            {
+                textBoxResult.Text = "요일만 입력해주세요! ex.수요일/";
+            }
+                
+        }
+    }
+}
