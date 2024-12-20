9f25d4cfeb2fec11876233d35c943830fd972322
Kim-yerin0904 <yeelin0904@naver.com>
2024-12-20 11:40:54 +0900

    for문 실습_어려운ver

--- example-241220/Form1.Designer.cs
+++ example-241220/Form1.Designer.cs
@@ -31,26 +31,34 @@
             this.textBoxInput = new System.Windows.Forms.TextBox();
             this.textBoxResult = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.studentName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.koreanScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.englishScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mathScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBoxInput
             // 
-            this.textBoxInput.Location = new System.Drawing.Point(32, 69);
+            this.textBoxInput.Location = new System.Drawing.Point(12, 70);
             this.textBoxInput.Name = "textBoxInput";
             this.textBoxInput.Size = new System.Drawing.Size(100, 25);
             this.textBoxInput.TabIndex = 0;
             // 
             // textBoxResult
             // 
-            this.textBoxResult.Location = new System.Drawing.Point(32, 115);
+            this.textBoxResult.Location = new System.Drawing.Point(12, 115);
             this.textBoxResult.Multiline = true;
             this.textBoxResult.Name = "textBoxResult";
-            this.textBoxResult.Size = new System.Drawing.Size(299, 205);
+            this.textBoxResult.Size = new System.Drawing.Size(217, 307);
             this.textBoxResult.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(167, 69);
+            this.button1.Location = new System.Drawing.Point(134, 72);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -58,16 +66,87 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.studentName,
+            this.koreanScore,
+            this.englishScore,
+            this.mathScore});
+            this.dataGridView1.Location = new System.Drawing.Point(248, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(540, 364);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "가짜 성적표 만들기~";
+            // 
+            // studentName
+            // 
+            this.studentName.HeaderText = "학생 이름";
+            this.studentName.MinimumWidth = 6;
+            this.studentName.Name = "studentName";
+            this.studentName.ReadOnly = true;
+            this.studentName.Width = 125;
+            // 
+            // koreanScore
+            // 
+            this.koreanScore.HeaderText = "국어 성적";
+            this.koreanScore.MinimumWidth = 6;
+            this.koreanScore.Name = "koreanScore";
+            this.koreanScore.ReadOnly = true;
+            this.koreanScore.Width = 125;
+            // 
+            // englishScore
+            // 
+            this.englishScore.HeaderText = "영어 성적";
+            this.englishScore.MinimumWidth = 6;
+            this.englishScore.Name = "englishScore";
+            this.englishScore.ReadOnly = true;
+            this.englishScore.Width = 125;
+            // 
+            // mathScore
+            // 
+            this.mathScore.HeaderText = "수학 성적";
+            this.mathScore.MinimumWidth = 6;
+            this.mathScore.Name = "mathScore";
+            this.mathScore.ReadOnly = true;
+            this.mathScore.Width = 125;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(590, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "성적표 .csv 파일로 저장";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBoxResult);
             this.Controls.Add(this.textBoxInput);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,6 +157,13 @@
         private System.Windows.Forms.TextBox textBoxInput;
         private System.Windows.Forms.TextBox textBoxResult;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn koreanScore;
+        private System.Windows.Forms.DataGridViewTextBoxColumn englishScore;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mathScore;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- example-241220/Form1.cs
+++ example-241220/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,22 +18,102 @@ namespace example_241220
             InitializeComponent();
         }
 
-        void MakeResultSentence (string studentName, int Score)
+        void MakeResultSentence (string studentName, int koreanScore, int englishScore, int mathScore)
         {
-            textBoxResult.Text += studentName + ":" + Score + "점\r\n";
+            textBoxResult.Text += studentName + "\r\n";
+            textBoxResult.Text += "국어 :" + koreanScore + "/ 영어 :" + englishScore + "/ 수학 :" + mathScore + "\r\n";
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        { 
             textBoxResult.Text = "";
-            int studentCount = int.Parse(textBoxInput.Text);
-            Random rand = new Random();
-            for (int i = 1; i < studentCount+1; i++)
+            int studentCount = 0;
+            int[,] scoreTable = new int[studentCount, 3];
+            // 숫자가 아닌 경우 예외 처리 (TryParse는 out 변수로 값이 들어감)
+            if (int.TryParse(textBoxInput.Text, out studentCount))
             {
-                int score = rand.Next(101);
-                string studentName = "학생" + i.ToString();
-                MakeResultSentence(studentName, score);
+                Random rand = new Random();
+                for (int i = 1; i < studentCount + 1; i++)
+                {
+                    int koreanScore = rand.Next(101);
+                    int englishScore = rand.Next(101);
+                    int mathScore = rand.Next(101);
+                    string studentName = "학생" + i.ToString();
+                    MakeResultSentence(studentName, koreanScore, englishScore, mathScore);
+                    dataGridView1.Rows.Add(studentName, koreanScore, englishScore, mathScore);
+                }
             }
+            else
+            {
+                textBoxResult.Text = "숫자만 입력해 주세요!";
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV 파일 |*.csv|모든 파일|*.*";
+            saveFileDialog.FilterIndex = 1;
+            // 대화상자를 닫기 전에 디렉토리를 이전에 선택한 디렉토리로 복원한지의 여부를 나타냄
+            saveFileDialog.RestoreDirectory = true;
+            // 확장명을 입려하지 않을 경우, 자동으로 csv로 지정
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.DefaultExt = "csv";
+            // 파일이 이미 존재하면 덮어쓰기를 할지 묻는 대화상자 표시 기본값=true
+            saveFileDialog.OverwritePrompt = true;
+            // 저장할 위치의 초기 디렉토리 설정
+            saveFileDialog.InitialDirectory = Environment.CurrentDirectory;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                DataGridViewToCSV(saveFileDialog.FileName, dataGridView1, true);
+            }
+        }
+        private void DataGridViewToCSV(string fileName, DataGridView dgview, bool header)
+        {
+            string delimiter = ","; //구분자
+            FileStream fs = new FileStream(fileName, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+            StreamWriter csvExport = new StreamWriter(fs, System.Text.Encoding.UTF8);
+
+            if (dgview.Rows.Count == 0) return;
+
+            // header 포함
+            if (header)
+            {
+                for (int i = 0; i < dgview.Columns.Count; i++)
+                {
+                    csvExport.Write(dgview.Columns[i].HeaderText);
+                    if (i != dgview.Columns.Count - 1)
+                    {
+                        csvExport.Write(delimiter);
+                    }
+                }
+            }
+            csvExport.Write(csvExport.NewLine); //add new line
+
+            //데이터 출력
+            foreach (DataGridViewRow row in dgview.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    for (int i = 0; i < dgview.Columns.Count; i++)
+                    {
+                        csvExport.Write(row.Cells[i].Value);
+                        if (i != dgview.Columns.Count - 1)
+                        {
+                            csvExport.Write(delimiter);
+                        }
+                    }
+                    csvExport.Write(csvExport.NewLine); //write new line
+                }
+            }
+            csvExport.Flush();
+            csvExport.Close();
+            fs.Close();
+
+            MessageBox.Show("CSV 파일 저장 완료!");
         }
     }
 }
+
