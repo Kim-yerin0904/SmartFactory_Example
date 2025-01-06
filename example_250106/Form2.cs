aee8293e716bcdc12e60142681d10ce62a0d03c1
Kim-yerin0904 <yeelin0904@naver.com>
2025-01-06 12:26:43 +0900

    250106 backgroundworker 실습 수정

--- example_250106/Form2.cs
+++ example_250106/Form2.cs
@@ -26,26 +26,25 @@ namespace example_250106
 
             this.worker.DoWork += new DoWorkEventHandler(search);
             this.worker.ProgressChanged += new ProgressChangedEventHandler(result);
+        }
+        void search(object sender, DoWorkEventArgs e)
+        {
+            string path = textBox_directory.Text;
+            string type = textBox_type.Text;
+            files = Directory.GetFiles(path, type, SearchOption.AllDirectories);
+            for (int i = 0; i < files.Length; i++)
+            {
+                this.worker.ReportProgress(i);
+            }
+        }
+        void result(object sender, ProgressChangedEventArgs e)
+        {
+            listBox1.Items.Add(files[e.ProgressPercentage]);
+        }
 
-            void search(object sender, DoWorkEventArgs e)
-            {
-                string path = textBox_directory.Text;
-                string type = textBox_type.Text;
-                files = Directory.GetFiles(path,type,SearchOption.AllDirectories);
-                for (int i = 0; i < files.Length; i++)
-                {
-                    this.worker.ReportProgress(i);
-                }
-            }
-            void result(object sender, ProgressChangedEventArgs e)
-            {
-                listBox1.Items.Add(files[e.ProgressPercentage]);
-            }
-            
-            void complete(object sender, EventArgs e)
-            {
-                MessageBox.Show("검색 완료!");
-            }
+        void complete(object sender, EventArgs e)
+        {
+            MessageBox.Show("검색 완료!");
         }
 
         private void input_button_Click(object sender, EventArgs e)
