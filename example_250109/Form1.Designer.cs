fc6027d6e8b525dd7c9a49fb95e620a33f6f97cc
Kim-yerin0904 <yeelin0904@naver.com>
2025-01-09 11:28:42 +0900

    250109 delegate 실습

---
+++ example_250109/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace example_250109
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addevent_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.remove_button = new System.Windows.Forms.Button();
+            this.callevent_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addevent_button
+            // 
+            this.addevent_button.Location = new System.Drawing.Point(162, 35);
+            this.addevent_button.Name = "addevent_button";
+            this.addevent_button.Size = new System.Drawing.Size(121, 98);
+            this.addevent_button.TabIndex = 0;
+            this.addevent_button.Text = "이벤트 추가";
+            this.addevent_button.UseVisualStyleBackColor = true;
+            this.addevent_button.Click += new System.EventHandler(this.addevent_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(269, 164);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 226);
+            this.textBox1.TabIndex = 1;
+            // 
+            // remove_button
+            // 
+            this.remove_button.Location = new System.Drawing.Point(333, 35);
+            this.remove_button.Name = "remove_button";
+            this.remove_button.Size = new System.Drawing.Size(115, 98);
+            this.remove_button.TabIndex = 2;
+            this.remove_button.Text = "이벤트 삭제";
+            this.remove_button.UseVisualStyleBackColor = true;
+            this.remove_button.Click += new System.EventHandler(this.remove_button_Click);
+            // 
+            // callevent_button
+            // 
+            this.callevent_button.Location = new System.Drawing.Point(511, 35);
+            this.callevent_button.Name = "callevent_button";
+            this.callevent_button.Size = new System.Drawing.Size(115, 98);
+            this.callevent_button.TabIndex = 3;
+            this.callevent_button.Text = "이벤트 실행";
+            this.callevent_button.UseVisualStyleBackColor = true;
+            this.callevent_button.Click += new System.EventHandler(this.callevent_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.callevent_button);
+            this.Controls.Add(this.remove_button);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.addevent_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addevent_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button remove_button;
+        private System.Windows.Forms.Button callevent_button;
+    }
+}
+
---
+++ example_250109/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static example_250109.Form1;
+
+namespace example_250109
+{
+    public delegate void EventDelegate(string eventname);
+
+    public partial class Form1 : Form
+    {
+        EventManager eventManager = new EventManager();
+        public Form1()
+        {
+            InitializeComponent();
+
+            eventManager.AddEvent("sts", event1);
+            eventManager.EventCall("sts", "메시지!");
+        }
+
+        public void event1(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+        public void event2(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+        #region 버튼
+        private void addevent_button_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void remove_button_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void callevent_button_Click(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+    }
+    public class EventManager
+    {
+        Dictionary<string, EventDelegate> events = new Dictionary<string, EventDelegate>();
+
+        public void AddEvent(string eventname, EventDelegate e)
+        {
+            events.Add(eventname, e); // sts, evnet1()
+        }
+
+        public void RemoveEvent(string eventname)
+        {
+            events.Remove(eventname);
+        }
+        public void EventCall(string eventname, string msg) => events[eventname].Invoke(msg); // sts, msg
+    }
+}
