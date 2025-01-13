b30b26a6881ee8123f213370c083a05020f407c3
Kim-yerin0904 <yeelin0904@naver.com>
2025-01-13 11:44:46 +0900

    250113 MVVM 모델 실습

---
+++ example_250113/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace example_250113
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Views.MainWindow main = new();
+            main.ShowDialog();
+        }
+    }
+
+}
