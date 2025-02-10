bbe8a1262d614f43700c3e17e910dcb7731b851e
Kim-yerin0904 <yeelin0904@naver.com>
2025-02-10 11:47:24 +0900

    250210 OpenCV 시작, 패키지 설치 및 버전 확인

---
+++ example_250210/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenCvSharp;
+
+namespace example_250210
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textbox.Text = Cv2.GetVersionString();
+            Mat image = Cv2.ImRead("C:\\Users\\yeeli\\source\\repos\\example_241218\\example_250210\\disney.jpg");
+            Cv2.ImShow("Image test", image);
+            imageBox.Source = OpenCvSharp.WpfExtensions.BitmapSourceConverter.ToBitmapSource(image);
+            
+        }
+    }
+}
