22e963d028a5e28d8a0896d4a88171309811ba9e
Kim-yerin0904 <yeelin0904@naver.com>
2025-02-11 12:58:47 +0900

    250211 웹캠 실습

---
+++ example_250211/webcam.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OpenCvSharp;
+
+namespace example_250211
+{
+    /// <summary>
+    /// Interaction logic for videocapture.xaml
+    /// </summary>
+    public partial class webcam : System.Windows.Window
+    {
+        public webcam()
+        {
+            InitializeComponent();
+
+            VideoCapture capture = new VideoCapture(0);
+            Mat frame = new Mat();
+
+            capture.Set(VideoCaptureProperties.FrameWidth, 640);
+            capture.Set(VideoCaptureProperties.FrameHeight, 480);
+
+            while (true)
+            {
+                if (capture.IsOpened() == true)
+                {
+                    capture.Read(frame);
+                    Cv2.ImShow("Webcam", frame);
+
+                    if (Cv2.WaitKey(33) == 'q') break;
+                }
+            }
+
+            capture.Release();
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
